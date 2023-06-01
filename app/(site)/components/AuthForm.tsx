e280dbc6e53ca505015eba8e3fdff5b79bc4a5ad
khanhthanhdev <thanhkt27507@gmail.com>
2023-06-01 14:58:49 +0700

    github and google auth

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -10,7 +10,8 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Input from "@/app/components/inputs/Input";
 import AuthSocialButton from './AuthSocialButton';
 import Button from "@/app/components/Button";
-// import { toast } from "react-hot-toast";
+import { toast } from "react-hot-toast";
+import {signIn} from 'next-auth/react'
 
 
 type Variant = 'LOGIN' | 'REGISTER';
@@ -48,14 +49,41 @@ const AuthForm = () => {
 
         if (variant === 'REGISTER') {
             axios.post('/api/register', data)
+            .catch(() => toast.error("Something went wrong"))
+            .finally(() => setIsLoading(false));
         }
         if (variant === 'LOGIN') {
+            signIn('credentials', {
+                ...data,
+                redirect: false
+            })
+            .then((callback) => {
+                if (callback ?. error) {
+                    toast.error('Invalid credentials')
+                }
 
+                if (callback ?. ok && !callback?.error) {
+                    toast.success('Logged in')
+                }
+            })
+            .finally(() => setIsLoading(false));
         }
     }
 
     const socialAction = (action: string) => {
         setIsLoading(true);
+        
+        signIn(action, { redirect: false})
+        .then((callback) => {
+            if (callback?.error) {
+                toast.error('Invalid credentials')
+            }
+
+            if (callback?. ok && !callback?.error){
+                toast.success('Logged in')
+            }
+        })
+        .finally(() => setIsLoading(false))
 
     }
 
